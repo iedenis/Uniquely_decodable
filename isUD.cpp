d2bf0e1ee398ad1f7706e1cea1ed4ba5369cdbd1
Denis <denis.ievlev@gmail.com>
2015-11-13 17:11:30 +0200

    Cleaning the code and adding the comments

--- isUD.cpp
+++ isUD.cpp
@@ -1,9 +1,8 @@
 //============================================================================
 // Name        : isUD.cpp
-// Author      :
-// Version     :
+// Author      : Denis Ievlev & Lior Shmuel
 // Copyright   :
-// Description : This program is checking the input code if it uniquely decodable
+// Description : This program is checking the input code if it is uniquely decodable
 //============================================================================
 
 #include <iostream>
@@ -13,24 +12,22 @@
 #include <sstream>
 #include <bitset>
 
-using namespace std;
 std::vector<std::pair<int, int> > vectorOfSets; //vector of input alphabet as a set
 std::vector<std::pair<int, int> > C1; //first set
 std::vector<std::pair<int, int> > C2; //second set
 
-bool contains(vector<pair<int, int>> &vec, pair<int, int> elem) {
-	for (std::vector<pair<int, int> >::iterator iter = vec.begin();
+bool contains(std::vector<std::pair<int, int>> &vec, std::pair<int, int> elem) {
+	for (std::vector<std::pair<int, int> >::iterator iter = vec.begin();
 			iter != vec.end(); ++iter) {
 		if (*iter == elem)
 			return true;
 	}
 	return false;
-
 }
-bool isPrefix(pair<int, int> p1, pair<int, int> p2,
-		vector<pair<int, int>> &vec) {
-	int v1 = 1;
-	int v2 = 1;
+
+bool isPrefix(std::pair<int, int> p1, std::pair<int, int> p2,
+		std::vector<std::pair<int, int>> &vec) {
+	int v1 = 1, v2 = 1;
 	int minLength, maxLength;
 	if (p1.first < p2.first) {
 		minLength = p1.first;
@@ -46,6 +43,10 @@ bool isPrefix(pair<int, int> p1, pair<int, int> p2,
 	for (int i = 0; i < p2.first - 1; ++i) {
 		v2 <<= 1;
 	}
+	/*
+	 * checking if the short number is prefix of the long number
+	 * returns false when it is not a prefix.
+	 */
 	for (int i = minLength; i > 0; i--) {
 		if (((((v1 & p1.second) == 0) && ((v2 & p2.second) == 0)))
 				|| ((((v1 & p1.second) != 0)) && ((v2 & p2.second) != 0))) {
@@ -53,90 +54,104 @@ bool isPrefix(pair<int, int> p1, pair<int, int> p2,
 			v2 >>= 1;
 		} else
 			return false;
-
 	}
+	/*
+	 * If the short number is prefix of the long number,  add the
+	 * suffix of the long one to the set of suffixes
+	 * returns true, cause the short number is prefix of the long.
+	 */
 	v1 = 1;
 	for (int i = 0; i < maxLength - minLength - 1; ++i) {
 		v1 = (v1 << 1) + 1;
 	}
-	//v1 = (maxLength-minLength+1) | 1;
 	int elem = v1 & ((p1.first > p2.first) ? p1.second : p2.second);
-	if (!contains(vec, make_pair(maxLength - minLength, elem))) {
-		vec.push_back(make_pair(maxLength - minLength, elem));
+	if (!contains(vec, std::make_pair(maxLength - minLength, elem))) {
+		vec.push_back(std::make_pair(maxLength - minLength, elem));
 	}
-
 	return true;
 }
-void printPair(pair<int, int> a) {
-	cout << "(" << a.first << "," << std::bitset<5>(a.second) << ")";
+/*
+ * Prints one pair of the set
+ * The first number represents the length of the word
+ * The second number represents the word in decimal
+ */
+void printPair(std::pair<int, int> a) {
+	std::cout << "(" << a.first << "," << std::bitset<5>(a.second) << ")";
 }
+/*
+ * Parsing the input string.
+ */
 std::pair<int, int> parse(std::string &str) {
 	std::istringstream st(str);
-	string first;
-	string second = "";
+	std::string first;
+	std::string second = "";
 	std::getline(st, first, ',');
 	first = first.substr(1);
 	std::getline(st, second);
 	second = second.substr(1, 2);
 	int num1 = std::stoi(first);
 	int num2 = std::stoi(second);
-	return make_pair(num1, num2);
+	return std::make_pair(num1, num2);
 }
 
 int main(int argc, char *argv[]) {
-	//pair <int,int> a= make_pair(4,2);
-	//pair <int , int>b=make_pair(3,2);
-	ifstream file;
+	if (argc != 2) {
+		std::cerr
+				<< "Not valid number of parameters. Please insert only to the path to the input file"
+				<< std::endl;
+		exit(0);
+	}
+	std::ifstream file;
 	file.open(argv[1]);
-	string myVec;
-	int i = 0;
+	std::string myVec; //input lines
 	if (!file.is_open())
-		cout << "can't open the file" << endl;
+		std::cerr << "Can't open the file. Please check the path to the file"
+				<< std::endl;
 	else
 		while (getline(file, myVec)) {
-			vectorOfSets.push_back(parse(myVec));
-			i++;
+			if (myVec != "") {
+				vectorOfSets.push_back(parse(myVec));
+			}
 		}
 	file.close();
 
-	for (std::vector<pair<int, int> >::iterator it = vectorOfSets.begin();
+	for (std::vector<std::pair<int, int> >::iterator it = vectorOfSets.begin();
 			it != vectorOfSets.end(); ++it) {
-		for (std::vector<pair<int, int> >::iterator iter = it;
+		for (std::vector<std::pair<int, int> >::iterator iter = it;
 				iter != vectorOfSets.end(); ++iter) {
-
 			if ((iter + 1) != vectorOfSets.end()) {
 				isPrefix(*it, *(iter + 1), C1);
 			}
 		}
 	}
 	//print C1
-	cout << "C1 vector contains" << endl;
+	std::cout << "C1 vector contains" << std::endl;
 
-	for (std::vector<pair<int, int> >::iterator iter = C1.begin();
+	for (std::vector<std::pair<int, int> >::iterator iter = C1.begin();
 			iter != C1.end(); ++iter) {
 		printPair(*iter);
 	}
-	cout << endl;
-	for (std::vector<pair<int, int> >::iterator it = vectorOfSets.begin();
+	std::cout << std::endl;
+	for (std::vector<std::pair<int, int> >::iterator it = vectorOfSets.begin();
 			it != vectorOfSets.end(); ++it) {
-		for (std::vector<pair<int, int> >::iterator iter = C1.begin();
+		for (std::vector<std::pair<int, int> >::iterator iter = C1.begin();
 				iter != C1.end(); ++iter) {
 			isPrefix(*it, *iter, C2);
 		}
 	}
 
 	//print C2
-	cout << "C2 vector contains" << endl;
-	for (std::vector<pair<int, int> >::iterator iter = C2.begin();
+	std::cout << "C2 vector contains" << std::endl;
+	for (std::vector<std::pair<int, int> >::iterator iter = C2.begin();
 			iter != C2.end(); ++iter) {
 		printPair(*iter);
 	}
-	cout << "\n**************************************" << endl;
+	std::cout << "\n**************************************" << std::endl;
 	if (C2.size() > 0)
-		cout << "* The code is not uniquely decodable *" << endl;
+		std::cout << "* The code is not uniquely decodable *" << std::endl;
 	else
-		cout << "* The code is uniquely decodable     *" << endl;
-	cout << "**************************************" << endl;
+		std::cout << "* The code is uniquely decodable     *" << std::endl;
+	std::cout << "**************************************" << std::endl;
 	return 0;
 }
 
